b5da3b06c1f4bba0b6279670f7bea971b9877fa3
Boris Pouw <boris.pouw@cm.com>
2023-05-11 23:44:01 +0200

    goodnight

--- Webshop/Controllers/OrderController.cs
+++ Webshop/Controllers/OrderController.cs
@@ -1,8 +1,35 @@
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Webshop.Commands;
+using Webshop.Interfaces;
 
 namespace Webshop.Controllers;
 
+
+[ApiController]
+[Route("api/")]
 public class OrderController : ControllerBase
 {
-    
+    private readonly ICommandHandler<CreateOrderCommand, Order> _createOrderCommandHandler;
+
+    public OrderController(ICommandHandler<CreateOrderCommand, Order> createOrderCommandHandler)
+    {
+        _createOrderCommandHandler = createOrderCommandHandler;
+    }
+
+    [HttpPost("orders")]
+    public async Task<IActionResult> CreateOrder(Order order)
+    {
+        var command = new CreateOrderCommand()
+        {
+            CustomerId = order.CustomerId,
+            Psp = order.Psp,
+            OrderProducts = order.OrderProducts
+        };
+
+        Order resultOrder = await _createOrderCommandHandler.Handle(command, CancellationToken.None);
+
+        return Created("Succes", resultOrder);
+    }
+
 }
--- Webshop/Program.cs
+++ Webshop/Program.cs
@@ -2,17 +2,37 @@ using Domain;
 using Domain.Service.IRepository;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Webshop.Commands;
+using Webshop.Handlers;
+using Webshop.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var dbString = builder.Configuration.GetConnectionString("WebshopDB") ?? throw new InvalidOperationException("Connection string 'WebshopConnection' not found.");
+
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<WebshopDbContext>(options =>
     options.UseMySQL(dbString));
 
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
+builder.Services.AddScoped<ICommandHandler<CreateOrderCommand, Order>, CreateOrderCommandHandler>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 app.MapGet("/", () => "Hello World!");
 
